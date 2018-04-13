b3182449a1ae94c3278267703f7228f10a255893
my <my@my.com>
2018-04-13 20:49:20 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,45 @@
+var app = getApp();
+Page({
+  data: {
+    winHeight: "",//窗口高度
+    currentTab: 0, //预设当前项的值
+    scrollLeft: 0, //tab标题的滚动条位置
+    condition: [{ con: "一人食" }, { con: "增肥" }, { con: "外卖" }, { con: "￥10-25" }]
+
+  },
+  // 滚动切换标签样式
+  switchTab: function (e) {
+    this.setData({
+      currentTab: e.detail.current
+    });
+    // this.checkCor();
+  },
+  // 点击标题切换当前页时改变样式
+  swichNav: function (e) {
+    var cur = e.target.dataset.current;
+    if (this.data.currentTaB == cur) { return false; }
+    else {
+      this.setData({
+        currentTab: cur
+      })
+    }
+  },
+
+  onLoad: function () {
+    var that = this;
+    //  高度自适应
+    wx.getSystemInfo({
+      success: function (res) {
+        var clientHeight = res.windowHeight,
+          clientWidth = res.windowWidth,
+          rpxR = 750 / clientWidth;
+        var calc = clientHeight * rpxR - 180;
+        console.log(calc)
+        that.setData({
+          winHeight: calc
+        });
+      }
+    });
+  },
+  footerTap: app.footerTap
+})
---
+++ pages/storeDetail/index.js
@@ -0,0 +1,9 @@
+
+Page({
+  data: {
+    condition: [{ con: "一人食" }, { con: "增肥" }, { con: "外卖" }, { con: "￥10-25" }]
+  },
+  onLoad: function () {
+
+  }
+})
