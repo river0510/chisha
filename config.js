210ece1118905274cba4424c86b21d9f0a67f63c
huanghe <huanghe@huanghe.local>
2018-05-08 15:42:02 +0800

    ..

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports={
-  // api:'http://119.28.74.251:4399/api/v1/'
-  api:'http://localhost:4000/api/v1/'
+  api:'http://119.28.74.251:4399/api/v1/'
+  // api:'http://localhost:4000/api/v1/'
 }
